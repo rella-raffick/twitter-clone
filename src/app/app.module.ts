f54e8ea2c37879dd3a11a6de5774c29217354754
Vigneshvelmurugann <vigneshvelmurugann@gmail.com>
2022-03-23 21:12:31 +0530

    sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MiddleFeedComponent } from './middle-feed/middle-feed.component';
+import { WhatsHappeningComponent } from './whats-happening/whats-happening.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MiddleFeedComponent,
+    WhatsHappeningComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/middle-feed/middle-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-feed',
+  templateUrl: './middle-feed.component.html',
+  styleUrls: ['./middle-feed.component.scss']
+})
+export class MiddleFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
