4eaacffc3610085816292caf6a2525728d0c8f0d
su-bashree10 <subhasreeassociates@gmail.com>
2022-03-24 21:41:40 +0530

    Home-Tweets Display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewtweetComponent } from './newtweet/newtweet.component';
+import { DisplayfeedComponent } from './displayfeed/displayfeed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewtweetComponent
+    NewtweetComponent,
+    DisplayfeedComponent
   ],
   imports: [
     BrowserModule,
