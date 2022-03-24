602b3a883d45d1b8eca0125585b94135a8453552
Vigneshvelmurugann <vigneshvelmurugann@gmail.com>
2022-03-24 15:12:09 +0530

    side and top nav completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { MiddleFeedComponent } from './middle-feed/middle-feed.component';
-import { WhatsHappeningComponent } from './whats-happening/whats-happening.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { MiddleFeedComponent } from './components/middle-feed/middle-feed.component';
+import { WhatsHappeningComponent } from './components/whats-happening/whats-happening.component';
 
 @NgModule({
   declarations: [
