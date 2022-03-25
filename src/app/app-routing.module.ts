a4674898e1d18f8ae8479cf361f6e42d5303d57a
PREMSR0202 <srprem2000@gmail.com>
2022-03-25 09:40:50 +0530

    Completed Message left Section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { CompleteComponent } from './components/message/complete/complete.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'message', pathMatch: 'full' },
+  { path: 'message', component: CompleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CompleteComponent } from './components/message/complete/complete.component';
+import { MsgnavbarComponent } from './components/message/msgnavbar/msgnavbar.component';
+import { MsgsearchbarComponent } from './components/message/msgsearchbar/msgsearchbar.component';
+import { ChatsComponent } from './components/message/chats/chats.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompleteComponent,
+    MsgnavbarComponent,
+    MsgsearchbarComponent,
+    ChatsComponent,
   ],
   imports: [
     BrowserModule,
