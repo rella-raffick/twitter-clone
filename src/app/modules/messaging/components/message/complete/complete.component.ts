edc579a88f29faaaba8b3dc827318f88030f2663
rella-raffick <rellaraffickitvec@gmail.com>
2022-03-26 20:57:36 +0530

    Merge

---
+++ src/app/modules/messaging/components/message/complete/complete.component.ts
@@ -0,0 +1,32 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complete',
+  templateUrl: './complete.component.html',
+  styleUrls: ['./complete.component.scss']
+})
+export class CompleteComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private navigate: Router) { }
+
+  chats: number[] = [1, 2]
+
+  paramId: string = '';
+
+  ngOnInit(): void {
+  }
+
+  getActivatedId(id: any) {
+    if (id.route) {
+      this.paramId = id.route.params._value.id
+    } else {
+      this.paramId = ''
+    }
+  }
+
+  chatBoxNavigate() {
+    this.navigate.navigate(['/messaging/message/1'])
+  }
+
+}
