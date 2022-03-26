ff19ccbe3444fbbd796f99363d0e6f5309cda3da
sridharshankar25 <sridhar25600@gmail.com>
2022-03-26 10:36:47 +0530

    notification completed

--- src/app/components/Notification/notification-mention/notification-mention.component.ts
+++ src/app/components/Notification/notification-mention/notification-mention.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationMentionComponent implements OnInit {
 
+  arrayReply:boolean = false;
+  arrayRetweet:boolean=false;
+  arrayLikes:boolean=false;
+  like:number = 21;
+  retweet :string ="Retweet";
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  increaseReplyCount(){
+      this.arrayReply = !this.arrayReply;
+  }
+  increaseRetweetCount(){
+      this.arrayRetweet = !this.arrayRetweet;
+      if(this.retweet === "Retweet"){
+      this.retweet = "undo Retweet";
+      }
+      else{
+        this.retweet ="Retweet";
+      }
+  }
+  increaseLikesCount(){
+    if(this.arrayLikes === true){
+        this.like = this.like - 1;
+    }  
+    else{
+      this.like = this.like + 1;
+    }
+    this.arrayLikes = !this.arrayLikes;
+      
+      
+  }
+
+
+
 
 }
