2c849f4c2f4c9f6ee0ab72e00e2fcba627342401
rella-raffick <rellaraffickitvec@gmail.com>
2022-03-27 13:39:05 +0530

    Revert to Bootstrap Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotificationComponent } from './components/Notification/notification/notification.component';
 
-const routes: Routes = [
-   { path: 'home', component:HomeComponent},
-  { path: 'notification', component: NotificationComponent },
-  { path: 'notification/mentions', component: NotificationComponent },
-  {
-    path: 'messaging', loadChildren: () => import('./modules/messaging/messaging.module')
-      .then(m => m.MessagingModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,18 @@
-
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { MiddleFeedComponent } from './components/middle-feed/middle-feed.component';
-import { WhatsHappeningComponent } from './components/whats-happening/whats-happening.component';
-import { NewtweetComponent } from './components/newtweet/newtweet.component';
-import { DisplayfeedComponent } from './components/displayfeed/displayfeed.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NotificationComponent } from './components/Notification/notification/notification.component';
-import { NotificationNavComponent } from './components/Notification/notification-nav/notification-nav.component';
-import { NotificationMentionComponent } from './components/Notification/notification-mention/notification-mention.component';
-import { NotificationAllComponent } from './components/Notification/notification-all/notification-all.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideNavComponent,
-    MiddleFeedComponent,
-    WhatsHappeningComponent,
-    NewtweetComponent,
-    DisplayfeedComponent,
-    HomeComponent,
-    NotificationComponent,
-    NotificationNavComponent,
-    NotificationMentionComponent,
-    NotificationAllComponent
-
+    AppComponent
   ],
-    imports: [
+  imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
