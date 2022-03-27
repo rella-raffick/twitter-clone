06651b5ec55d960f2508fba57b51836b599ef451
PREMSR0202 <srprem2000@gmail.com>
2022-03-27 16:40:29 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotificationComponent } from './components/Notification/notification/notification.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'notification', component: NotificationComponent },
   { path: 'notification/mentions', component: NotificationComponent },
