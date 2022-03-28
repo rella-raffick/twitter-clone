dc2cb1b1734202ca082599e87f408f3fcfc49cd8
sridharshankar25 <sridhar25600@gmail.com>
2022-03-28 12:13:11 +0530

    profile merge with responsive

--- src/app/components/profiles/profile-likes/profile-likes.component.ts
+++ src/app/components/profiles/profile-likes/profile-likes.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileLikesComponent implements OnInit {
 
-  constructor() { }
+ 
+  array:any[] = [
+    {
+    "reply": 11,
+    "retweet": 19,
+    "likes": 76,
+    "replystatus" : false,
+    "retweetstatus" : false,
+    "likesstatus" : true,
+    "retweetvalue" : "Retweet"
 
-  ngOnInit(): void {
+  },
+
+    {"reply": 1,
+    "retweet": 15,
+    "likes": 97,
+    "replystatus" : false,
+    "retweetstatus" : false,
+    "likesstatus" : true,
+    "retweetvalue" : "Retweet"
   }
+  ];
+arrayReply:boolean = false;
+arrayRetweet:boolean=false;
+arrayLikes:boolean=false;
+like:number = 21;
+retweet :string ="Retweet";
+constructor() { }
 
+ngOnInit(): void {
+}
+
+increaseReplyCount(i : number){
+this.array[i].replystatus = !this.array[i].replystatus;
+}
+increaseRetweetCount(i : number){
+this.array[i].retweet =  this.array[i].retweet + 1;
+if(this.array[i].retweetstatus === false)
+{
+this.array[i].retweetvalue = "undo Retweet";
+}
+else{
+this.array[i].retweetvalue ="Retweet";
+}
+this.array[i].retweetstatus = !this.array[i].retweetstatus;
+}
+increaseLikesCount(i :number){
+if(this.array[i].likesstatus === true){
+this.array[i].likes = this.array[i].likes - 1;
+}  
+else{
+this.array[i].likes = this.array[i].likes + 1;
+}
+this.array[i].likesstatus = !this.array[i].likesstatus;
+
+
+}
 }
