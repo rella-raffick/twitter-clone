d07fc76659c40ea06f3f3f95188d6785ef5f55e2
sridharshankar25 <sridhar25600@gmail.com>
2022-03-29 14:41:03 +0530

    Explore Nav

--- src/app/components/explore/explore-nav/explore-nav.component.ts
+++ src/app/components/explore/explore-nav/explore-nav.component.ts
@@ -7,9 +7,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExploreNavComponent implements OnInit {
 
+  active:string = "foryou";
+  classForyou : Boolean = true ;
+  classCovid: Boolean = false;
+  classTrending : Boolean = false ;
+  classNews: Boolean = false;
+  classSports : Boolean = false ;
+  classEntertainment: Boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  forYou(){
+    
+  this.classForyou  = true ;
+  this.classCovid= false;
+  this.classTrending= false ;
+  this.classNews= false;
+  this.classSports= false ;
+  this.classEntertainment= false;
+  this.active = "foryou";
+  }
+
+    covid(){
+    
+      this.classForyou  = false ;
+      this.classCovid= true;
+      this.classTrending= false ;
+      this.classNews= false;
+      this.classSports= false ;
+      this.classEntertainment= false;
+      this.active = "covid";
+      }
+
+      trending(){
+    
+        this.classForyou  = false ;
+        this.classCovid= false;
+        this.classTrending= true ;
+        this.classNews= false;
+        this.classSports= false ;
+        this.classEntertainment= false;
+        this.active = "trending";
+        }
+
+        news(){
+    
+          this.classForyou  = false ;
+          this.classCovid= false;
+          this.classTrending= false ;
+          this.classNews= true;
+          this.classSports= false ;
+          this.classEntertainment= false;
+          this.active = "news";
+          }
+
+          sports(){
+    
+            this.classForyou  = false ;
+            this.classCovid= false;
+            this.classTrending= false ;
+            this.classNews= false;
+            this.classSports= true ;
+            this.classEntertainment= false;
+            this.active = "sports";
+            }
+
+            entertainment(){
+    
+              this.classForyou  = false ;
+              this.classCovid= false;
+              this.classTrending= false ;
+              this.classNews= false;
+              this.classSports= false ;
+              this.classEntertainment= true;
+              this.active = "entertainment";
+              }
 
 }
