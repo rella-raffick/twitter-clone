4985bddcbd7e73d5c6e243abb27351eb9b8f0edf
Vigneshvelmurugann <vigneshvelmurugann@gmail.com>
2022-03-30 09:42:27 +0530

    Merge branch 'features-navbars' of https://github.com/PREMSR0202/twitter-clone

--- src/app/components/explore/explore-all/explore-all.component.ts
+++ src/app/components/explore/explore-all/explore-all.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-explore-all',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExploreAllComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  navigateHome() {
+    this.router.navigate(['/home'])
+  }
+
+  navigateNotification() {
+    this.router.navigate(['/notification'])
+  }
+  navigateMessage() {
+    this.router.navigate(['/messaging/message'])
+  }
+  navigateProfile() {
+    this.router.navigate(['/profile'])
+  }
+  navigateExplore() {
+    this.router.navigate(['/explore'])
+  }
+
 
 }
